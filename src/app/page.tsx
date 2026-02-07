fe607d3114509e7065605d345de9acf66dfca968
msmubassir <masmubassir@gmail.com>
2026-02-07 07:36:42 +0600

    Set skills grid to 2 columns on mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -253,7 +253,7 @@ export default function Home() {
                 Always shipping, always learning
               </span>
             </div>
-            <div className="grid gap-3 sm:gap-4 sm:grid-cols-2 lg:grid-cols-3">
+            <div className="grid gap-3 grid-cols-2 lg:grid-cols-3">
               {skills.map((skill) => (
                 <div
                   key={skill}
